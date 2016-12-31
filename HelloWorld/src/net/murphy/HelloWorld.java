541461a2be093cb3fb9e3619a6fc94d361afab39
murph <murph@MurphyHPLaptop>
2016-12-31 11:46:09 -0500

    comments to the code

--- HelloWorld/src/net/murphy/HelloWorld.java
+++ HelloWorld/src/net/murphy/HelloWorld.java
@@ -1,9 +1,18 @@
 package net.murphy;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class HelloWorld.
+ */
 public class HelloWorld {
 
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		System.out.println("Hello World!");
 	}
 
